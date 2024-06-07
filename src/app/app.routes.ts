28d692e5cbdbbec3ad60277592713a9ca744ea46
abdullah408012 <abdullahbhatti0408@gmail.com>
2024-06-07 00:02:51 -0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:'header',
+        pathMatch:'full'
+    },
+    {
+        path:'header',
+        component:HeaderComponent
+    }
+];
